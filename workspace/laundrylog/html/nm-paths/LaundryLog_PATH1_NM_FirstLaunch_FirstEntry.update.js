f73ec958ab0952c8dded08baefd554b6d3e575fe
IvanTheGeek <git@IvanTheGeek.com>
2026-04-01 09:35:59 -0400

    workspace: refresh nm path artifact

--- workspace/laundrylog/html/nm-paths/LaundryLog_PATH1_NM_FirstLaunch_FirstEntry.update.js
+++ workspace/laundrylog/html/nm-paths/LaundryLog_PATH1_NM_FirstLaunch_FirstEntry.update.js
@@ -1,1 +1,1 @@
-window.__llNmPathUpdateManifest = { version: "nm-path::20260330213223784", updatedAt: "2026-03-30T21:32:23Z" };
+window.__llNmPathUpdateManifest = { version: "nm-path::20260401133422492", updatedAt: "2026-04-01T13:34:22Z" };
